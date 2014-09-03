58247f6e6b1069209c7398972bc4f1b246e7db4c
jpatel531 <jamie@notespublication.com>
2014-09-03 17:19:11 +0100

    #get_journeys takes into account options given

--- lib/tfl_jp.rb
+++ lib/tfl_jp.rb
@@ -21,7 +21,43 @@ module TFLJourneyPlanner
 		def get_journeys(options)
 			from = options[:from].gsub(" ", "+")
 			to = options[:to].gsub(" ", "+")
-			self.class.get(BASE_URI, query: {app_key: app_key, app_id: app_id, from: from, to: to, via: nil, nationalSearch: false, timeIs: 'Departing', alternativeCycle: false, alternativeWalking: true, applyHtmlMarkup: false})
+			via = options[:via]
+			national_search = options[:national_search] || false
+			time_is = options[:time_is] || "Departing"
+
+			journey_preference = options[:journey_preference]
+			mode = options[:mode]
+			from_name = options[:from_name]
+			to_name = options[:to_name]
+			via_name = options[:via_name]
+			max_transfer_minutes = options[:max_transfer_minutes]
+			max_walking_minutes = options[:max_walking_minutes]
+			cycle_preference = options[:cycle_preference]
+			adjustment = options[:adjustment]
+			alternative_cycle = options[:alternative_cycle] || false
+			alternative_walking = options[:alternative_walking] || true
+			apply_html_markup = options[:apply_html_markup] || false
+
+			self.class.get(BASE_URI, query: 
+				{app_key: app_key, 
+				app_id: app_id, 
+				from: from, 
+				to: to, 
+				via: via, 
+				nationalSearch: national_search, 
+				timeIs: time_is,
+				journeyPreference: journey_preference,
+				mode: mode,
+				fromName: from_name,
+				toName: to_name,
+				viaName: via_name,
+				maxTransferMinutes: max_transfer_minutes,
+				maxWalkingMinutes: max_walking_minutes,
+				cyclePreference: cycle_preference,
+				adjustment: adjustment,
+				alternativeCycle: alternative_cycle, 
+				alternativeWalking: alternative_walking, 
+				applyHtmlMarkup: apply_html_markup})
 		end
 
 	end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,14 +1,13 @@
 require_relative 'spec_helper'
 
 describe TFLJourneyPlanner::Client, vcr: true do 
-	# vcr_options = { :cassette_name => "hello", :record => :new_episodes }
 	
 
 	it "must work" do 
-		VCR.use_cassette "hello", record: :new_episodes do 
+		VCR.use_cassette "hello", record: :none do 
 			client = TFLJourneyPlanner::Client.new(app_id: ENV["TFL_ID"], app_key: ENV["TFL_KEY"])
 			journeys =  client.get_journeys(from: "tw14 9nt", to: "tw14 8ex")
-			expect(journeys['journeys'][0]['startDateTime']).to eq "2014-09-03T16:13:00"
+			expect(journeys['journeys'][0]['startDateTime']).to eq "2014-09-03T16:58:00"
 		end
 	end
 
