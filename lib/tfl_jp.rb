521c45dc2b32ea5ff5a8b249390703149616c4af
jpatel531 <jamie@notespublication.com>
2014-09-03 14:32:08 +0100

    VCR up and running

--- lib/tfl_jp.rb
+++ lib/tfl_jp.rb
@@ -8,20 +8,30 @@ end
 
 module TFLJourneyPlanner
   
-	# class Journey
 
-	# 	include HTTParty
+	module Journey
 
-	# 	base_uri 'api.tfl.gov.uk'
+		include HTTParty
 
-	# 	def get_user
-	# 		response = self.class.get("/users/jpatel531")
-	# 		JSON.parse response
-	# 	end
+		format :json
 
-	# end
+		# base_uri 'api.tfl.gov.uk'
+
+		def get_journeys(options)
+			from = options[:from]
+			to = options[:to]
+			# journey_uri = "/journey/journeyresults/%7bfrom%7d/to/%7bto%7d/via/%7bvia%7d"
+			self.class.get('http://api.tfl.gov.uk/Journey/JourneyResults/%7Bfrom%7D/to/%7Bto%7D/via/%7Bvia%7D?from=tw14+9nt&to=tw3+3tu&via=&nationalSearch=False&date=&time=&timeIs=Departing&journeyPreference=&mode=&accessibilityPreference=&fromName=&toName=&viaName=&maxTransferMinutes=&maxWalkingMinutes=&walkingSpeed=&cyclePreference=&adjustment=&bikeProficiency=&alternativeCycle=False&alternativeWalking=True&applyHtmlMarkup=False&app_id=&app_key=')
+			# self.class.get(journey_uri, query: {from: from, to: to, app_key: app_key})
+			# response = self.class.get("/users/jpatel531")
+		end
+
+	end
 
 	class Client
+
+		include HTTParty
+		include TFLJourneyPlanner::Journey
 
 		attr_reader :app_key, :app_id
 
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,10 +1,12 @@
-# require_relative 'spec_helper'
+require_relative 'spec_helper'
 
-# describe TFLJourneyPlanner::Journey, vcr: true do 
-# 	vcr_options = { :cassette_name => "hello", :record => :new_episodes }
+describe TFLJourneyPlanner::Client, vcr: true do 
+	vcr_options = { :cassette_name => "hello", :record => :new_episodes }
 
-# 	it "must work" do 
-		
-# 	end
+	it "must work" do 
+		client = TFLJourneyPlanner::Client.new(app_id: ENV["TFL_ID"], app_key: ENV["TFL_KEY"])
+		journeys =  client.get_journeys(from: "tw14 9nt", to: "tw14 8ex")
+		expect(journeys['journeys'][0]['startDateTime']).to eq "2014-09-03T14:39:00"
+	end
 
-# end
+end
