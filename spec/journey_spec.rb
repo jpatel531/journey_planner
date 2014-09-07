6d15687e276d3b720e218feefb3f9944d7a8841f
jpatel531 <jamie@notespublication.com>
2014-09-07 13:27:44 +0100

    Changed vcr request_matcher to method

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -6,14 +6,14 @@ describe TFLJourneyPlanner::Journey do
 	let(:journeys) {journeys =  client.get_journeys(from: "tw14 9nt", to: "tw14 8ex")}
 
 	it "should return an array of instructions" do 
-		VCR.use_cassette "hello", record: :none, match_requests_on: [:uri] do 
-			hash = {"Sep 7 2014 11:56 - Sep 7 2014 12:01"=>["Continue along Fruen Road for 143 metres (2 minutes, 8 seconds).", "Turn right on to Bedfont Lane, continue for 172 metres (2 minutes, 33 seconds)."], "Sep 7 2014 12:01 - Sep 7 2014 12:05"=>["H25 bus to Bedfont Library / H25 bus towards Hatton Cross"], "Sep 7 2014 12:05 - Sep 7 2014 12:08"=>["Continue along Staines Road for 64 metres (0 minutes, 57 seconds).", "Turn left on to Grovestile Waye, continue for 95 metres (1 minute, 21 seconds)."]}
+		VCR.use_cassette "hello", record: :none, match_requests_on: [:method] do 
+			hash = {"Sep 3 2014 16:13 - Sep 3 2014 16:18"=>["Continue along Fruen Road for 143 metres (2 minutes, 8 seconds).", "Turn right on to Bedfont Lane, continue for 172 metres (2 minutes, 33 seconds)."], "Sep 3 2014 16:18 - Sep 3 2014 16:21"=>["H25 bus to Bedfont Library / H25 bus towards Hatton Cross"], "Sep 3 2014 16:21 - Sep 3 2014 16:24"=>["Continue along Staines Road for 64 metres (0 minutes, 57 seconds).", "Turn left on to Grovestile Waye, continue for 95 metres (1 minute, 21 seconds)."]}
 			expect(journeys[0].instructions).to eq hash
 		end
 	end
 
 	it "should return a map path as an array of coordinates" do 
-		VCR.use_cassette "hello", record: :none, match_requests_on: [:uri] do 
+		VCR.use_cassette "hello", record: :none, match_requests_on: [:method] do 
 			expect(journeys[0].map_path).to eq [[51.45151025215, -0.41971520833],[51.45144462064, -0.41951598516],[51.45031573039, -0.4204904277],[51.45054644033, -0.42092861212],[51.45077734235, -0.42138118615],[51.45094358299, -0.42170646688],[51.45129461858, -0.42239957193],[51.45127683589, -0.42241457688], [51.45129461858, -0.42239957193],[51.45148859493,
         -0.42278147255],[51.45207032325, -0.42391280827],[51.45250417214, -0.42474702056],[51.45324177029,
         -0.42610332716],[51.45343573478, -0.42648525981],[51.45343573478, -0.42648525981],[51.45368497803,
